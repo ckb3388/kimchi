b28d2ba6fce0efe852ee18366261e6e9bc1ef579
최경빈 <ckb3725@naver.com>
2023-11-16 22:50:43 +0900

    a

--- kimchi/urls.py
+++ kimchi/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'kimchi'
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('index/', views.index, name='index'),
     path('textplay', views.textplay, name='textplay'),
     path('', views.reset, {'reset': True}, name='textplay_reset'),
     path('increase_score/<int:score_increment>/', views.increase_score, name='increase_score'),
